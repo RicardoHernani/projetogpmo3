069cc93db4eaee0fb97f6c3e9fb2f523b87a934d
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-05-24 11:28:47 -0300

    Testando a primeira classe de domínio - Paciente

--- src/main/java/com/chavesricardo/projetogpmo3/resources/PacienteResource.java
+++ src/main/java/com/chavesricardo/projetogpmo3/resources/PacienteResource.java
@@ -1,16 +1,28 @@
 package com.chavesricardo.projetogpmo3.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chavesricardo.projetogpmo3.domain.Paciente;
+
 @RestController
-@RequestMapping(value="/paciente")
+@RequestMapping(value="/pacientes")
 public class PacienteResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Paciente> listar() {
+	
+		Paciente pct1 = new Paciente(1, 11111111);
+		Paciente pct2 = new Paciente(2, 22222222);
+	
+		List<Paciente> lista = new ArrayList<>();
+		lista.add(pct1);
+		lista.add(pct2);
+	
+		return lista;
 	}
-
 }
