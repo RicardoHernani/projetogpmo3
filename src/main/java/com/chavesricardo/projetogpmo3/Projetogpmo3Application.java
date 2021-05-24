b806b2044d41e56a4f8b165f411ff689523bf595
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-05-23 23:19:07 -0300

    Projeto criado

---
+++ src/main/java/com/chavesricardo/projetogpmo3/Projetogpmo3Application.java
@@ -0,0 +1,13 @@
+package com.chavesricardo.projetogpmo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetogpmo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetogpmo3Application.class, args);
+	}
+
+}
