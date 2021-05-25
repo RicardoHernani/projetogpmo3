b2e217f6d554f57f44ad329baf72d4e09b8b478c
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-05-24 22:02:13 -0300

    Criando operação de instanciação para Paciente

--- src/main/java/com/chavesricardo/projetogpmo3/Projetogpmo3Application.java
+++ src/main/java/com/chavesricardo/projetogpmo3/Projetogpmo3Application.java
@@ -1,13 +1,33 @@
 package com.chavesricardo.projetogpmo3;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.chavesricardo.projetogpmo3.domain.Paciente;
+import com.chavesricardo.projetogpmo3.repositories.PacienteRepository;
+
 @SpringBootApplication
-public class Projetogpmo3Application {
+public class Projetogpmo3Application implements CommandLineRunner {
+	
+	@Autowired
+	private PacienteRepository pacienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projetogpmo3Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Paciente pct1 = new Paciente(null, 11111111);
+		Paciente pct2 = new Paciente(null, 22222222);
+		
+		pacienteRepository.saveAll(Arrays.asList(pct1, pct2));
+		
 	}
 
 }
