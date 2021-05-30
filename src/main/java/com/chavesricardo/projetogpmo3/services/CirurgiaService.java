ddc9a3a7310522539585cabf28c1b47b14974954
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-05-29 21:21:49 -0300

    Criação do endpoint /Cirurgia/{id}

---
+++ src/main/java/com/chavesricardo/projetogpmo3/services/CirurgiaService.java
@@ -0,0 +1,24 @@
+package com.chavesricardo.projetogpmo3.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chavesricardo.projetogpmo3.domain.Cirurgia;
+import com.chavesricardo.projetogpmo3.repositories.CirurgiaRepository;
+import com.chavesricardo.projetogpmo3.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CirurgiaService {
+
+	@Autowired
+	private CirurgiaRepository repo;
+	
+	public Cirurgia buscar(Integer id) {
+		Optional<Cirurgia> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cirurgia.class.getName()));
+	}
+	
+}
